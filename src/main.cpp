e448faad5b9a8646af81f25386f46efc3cc7cf5f
menardmax <maxence.menard@reseau.eseo.fr>
2023-07-24 16:30:22 +0100

    add wifi functions

--- src/main.cpp
+++ src/main.cpp
@@ -4,44 +4,36 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 
+#include "wifi.hpp"
 #include <esp_chip_info.h>
+#include <esp_err.h>
 #include <esp_flash.h>
+#include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <nvs_flash.h>
 #include <sdkconfig.h>
 #include <stdio.h>
 
-extern "C" void app_main(void) {
+static const char *TAG = "Main";
+
+extern "C" void app_main(void)
+{
   vTaskDelay(2000 / portTICK_PERIOD_MS);
-  printf("Hello world!\n");
-  /* Print chip information */
-  esp_chip_info_t chip_info;
-  uint32_t flash_size;
-  esp_chip_info(&chip_info);
-  printf("This is %s chip with %d CPU core(s), WiFi%s%s, ", CONFIG_IDF_TARGET,
-         chip_info.cores, (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-         (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+  ESP_LOGI(TAG, "ESP Start");
+  // Initialize NVS
+  ESP_LOGI(TAG, "Initialize Flash ...");
 
-  unsigned major_rev = chip_info.revision / 100;
-  unsigned minor_rev = chip_info.revision % 100;
-  printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-  if (esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-    printf("Get flash size failed");
-    return;
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+      ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+  {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
   }
+  ESP_ERROR_CHECK(ret);
+  ESP_LOGI(TAG, "Initialize Flash done");
 
-  printf("%luMB %s flash\n", flash_size / (1024 * 1024),
-         (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded"
-                                                       : "external");
-
-  printf("Minimum free heap size: %ld bytes\n",
-         esp_get_minimum_free_heap_size());
-
-  for (int i = 10; i >= 0; i--) {
-    printf("Restarting in %d seconds...\n", i);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
-  printf("Restarting now.\n");
-  fflush(stdout);
-  esp_restart();
+  wifi::init();
+  wifi::start();
 }
