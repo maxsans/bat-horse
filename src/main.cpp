58c29eb55f910deac43cfca1120d569868ee0587
menardmax <maxence.menard@reseau.eseo.fr>
2023-08-02 15:54:48 +0100

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 /*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ * @name Bat-Horse
  *
- * SPDX-License-Identifier: CC0-1.0
+ * @version 1.0.1
+ *
+ * @description Motion capture for horse
+ *
+ * @author Maxsans and Charls
+ *
  */
-
+#include "globals.hpp"
+#include "flash.hpp"
+#include "espnow.hpp"
 #include "wifi.hpp"
+
 #include <esp_chip_info.h>
 #include <esp_err.h>
 #include <esp_flash.h>
@@ -14,26 +22,44 @@
 #include <nvs_flash.h>
 #include <sdkconfig.h>
 #include <stdio.h>
+#include <string>
+#include <iostream>
 
 static const char *TAG = "Main";
+
+void callback(std::string name, std::string src)
+{
+  printf("ok callback ");
+}
 
 extern "C" void app_main(void)
 {
   vTaskDelay(2000 / portTICK_PERIOD_MS);
+  printf("\n\e[1;35m---------------------------------------------------------------------------------------------------------\e[0m\n");
+  printf("\e[1;35m|\e[1;34m\t\t\t\t\t\t\t \t\t\t\t\t\t\e[1;35m|\e[0m\n");
+  printf("\e[1;35m|\e[1;34m\t\t\t\t\t\t\t \t\t\t\t\t\t\e[1;35m|\e[0m\n");
+  printf("\e[1;35m|\e[1;34m\t\t\t\t\t\tESP Start \t\t\t\t\t\t\e[1;35m|\e[0m\n");
+  printf("\e[1;35m|\e[1;34m\t\t\t\t\t\t\t \t\t\t\t\t\t\e[1;35m|\e[0m\n");
+  printf("\e[1;35m|\e[1;34m\t\t\t\t\t\t\t \t\t\t\t\t\t\e[1;35m|\e[0m\n");
+  printf("\e[1;35m---------------------------------------------------------------------------------------------------------\e[0m\n");
+
   ESP_LOGI(TAG, "ESP Start");
-  // Initialize NVS
-  ESP_LOGI(TAG, "Initialize Flash ...");
 
-  esp_err_t ret = nvs_flash_init();
-  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
-      ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-  {
-    ESP_ERROR_CHECK(nvs_flash_erase());
-    ret = nvs_flash_init();
-  }
-  ESP_ERROR_CHECK(ret);
-  ESP_LOGI(TAG, "Initialize Flash done");
+  flash::init();
 
-  wifi::init();
+  wifi::init(static_cast<wifi::wifiMode>(WIFI_MODE));
   wifi::start();
+
+  espNow::init(&callback);
+  espNow::start();
+  int i = 0;
+#if !IS_BASE
+  for (;;)
+  {
+    std::string myString = std::to_string(i);
+    espNow::sendDataSensor(myString);
+    vTaskDelay(7 / portTICK_PERIOD_MS);
+    i++;
+  }
+#endif
 }
